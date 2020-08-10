075ee034d39140f40e160fec121a88e9b18b1256
Wavist <sommy.okafor@yahoo.com>
2020-08-10 11:47:02 -0700

    db config alteration

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'fitness');
 
+define('DB_SERVER', 'us-cdbr-east-02.cleardb.com');
+define('DB_USERNAME', 'bbb03609868fec');
+define('DB_PASSWORD', 'f0c1c2a9');
+define('DB_NAME', 'heroku_97fcbdc288f858b');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
