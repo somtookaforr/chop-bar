fe439308102f3f92c3cb458eb9786e2355956b82
Wavist <sommy.okafor@yahoo.com>
2021-09-09 00:23:20 +0100

    added userform.css for sw.js

--- service-worker.js
+++ service-worker.js
@@ -47,9 +47,9 @@ self.addEventListener('install', (e) => {
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (r) { return r; }
       const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
+      // const cache = await caches.open(cacheName);
+      // console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      // cache.put(e.request, response.clone());
       return response;
     })());
   });
