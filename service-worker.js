2757633e42532762205a15a9554f0af1855b2831
Wavist <sommy.okafor@yahoo.com>
2021-09-09 00:23:02 +0100

    added userform.css for sw.js

--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,11 @@ const appShellFiles = [
   './',
   './index.php',
   './login.php',
-  './signup.php'
+  './signup.php',
+  './assets/css/index.css',
+  './assets/css/user-forms.css',
+  './assets/css/footer.css',
+  './assets/img/mainlogoo.png'
 ];
  
 
@@ -43,10 +47,9 @@ self.addEventListener('install', (e) => {
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (r) { return r; }
       const response = await fetch(e.request);
-      // const cache = await caches.open(cacheName);
-      // console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      // cache.put(e.request, response.clone());
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
       return response;
     })());
   });
-  
