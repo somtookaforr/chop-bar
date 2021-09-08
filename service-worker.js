8db01578b90369cfeff373100622509eca4ce3bb
Wavist <sommy.okafor@yahoo.com>
2021-09-09 00:09:08 +0100

    added fetch event for sw.js

--- service-worker.js
+++ service-worker.js
@@ -23,16 +23,9 @@ const appShellFiles = [
   './',
   './index.php',
   './login.php',
-  './signup.php',
-  './assets/css/index.css',
-  './includes/footer.php',
-  './assets/css/footer.css',
-  './assets/img/mainlogoo.png',
-  './assets/img/M4E7X3z80PQ.png',
-  './assets/img/M4E7X3z80PQ (2).png',
-  './assets/img/M4E7X3z80PQ (3).png'
+  './signup.php'
 ];
-// 
+ 
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
@@ -44,15 +37,16 @@ self.addEventListener('install', (e) => {
   });
 
   // AUTO FETCH AND CACHE
-  // self.addEventListener('fetch', (e) => {
-  //   e.respondWith((async () => {
-  //     const r = await caches.match(e.request);
-  //     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-  //     if (r) { return r; }
-  //     const response = await fetch(e.request);
-  //     const cache = await caches.open(cacheName);
-  //     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-  //     cache.put(e.request, response.clone());
-  //     return response;
-  //   })());
-  // });
+  self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) { return r; }
+      const response = await fetch(e.request);
+      // const cache = await caches.open(cacheName);
+      // console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      // cache.put(e.request, response.clone());
+      return response;
+    })());
+  });
+  
